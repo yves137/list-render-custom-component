a1717d6bf6378863c9bb465a0b7a8dccd4b64288
yves137 <oneg2981@gmail.com>
2023-10-03 13:07:34 +0200

    update project

--- src/AnimalList.js
+++
@@ -1,7 +0,0 @@
-export function AnimalList({animals}){
-    return (
-        <ul className="ml-10 mt-10 ">
-          {animals.map((animal,i)=><li key={i} className="py-3 px-7 border max-w-[10rem]">{animal}</li>)}
-        </ul>
-      );
-}
